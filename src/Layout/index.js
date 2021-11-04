e7ae37267fa207d286524a8480eb840e59b76e83
Jason Jisu Lee <86587172+sensibleEngineer@users.noreply.github.com>
2021-11-04 18:47:44 -0400

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -16,22 +16,22 @@ function Layout() {
       <Header />
       <div className="container">
         <Switch>
-          <Route path = "/decks/new">
+          <Route exact path = "/decks/new">
             <CreateDeck />
           </Route>
-          <Route path = "/decks/:deckId/cards/:cardId/edit">
+          <Route exact path = "/decks/:deckId/cards/:cardId/edit">
             <EditCard />
           </Route>
-          <Route path = "/decks/:deckId/cards/new">
+          <Route exact path = "/decks/:deckId/cards/new">
             <AddCard />
           </Route>
-          <Route path = "/decks/:deckId/edit">
+          <Route exact path = "/decks/:deckId/edit">
             <EditDeck />
           </Route>
-          <Route path = "/decks/:deckId/study">
+          <Route exact path = "/decks/:deckId/study">
             <Study />
           </Route>
-          <Route path = "/decks/:deckId">
+          <Route exact path = "/decks/:deckId">
             <Deck />
           </Route>
           <Route exact path = "/">
